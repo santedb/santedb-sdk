f6d668e29bbdd97701233525620163b8fe54dc25
Justin Fyfe <justin@fyfesoftware.ca>
2019-09-03 17:35:36 +0700

    Updated refs for ASM info

--- AppletCompiler/Properties/AssemblyInfo.cs
+++ AppletCompiler/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.9.4.*")][assembly: AssemblyInformationalVersion("Chippewa")]
-[assembly: AssemblyVersion("1.52.0.*")]
-[assembly: AssemblyFileVersion("1.52.0.0")]
+[assembly: AssemblyVersion("1.67.0.*")]
+[assembly: AssemblyFileVersion("1.67.0.0")]
 [assembly: AssemblyInformationalVersion("Halifax")]
 
